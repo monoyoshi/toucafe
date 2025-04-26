8e44b1ae3db1c6c44b1bf8ac255b51738b18dbbf
kyu(ren) <41705282+monoyoshi@users.noreply.github.com>
2025-04-26 03:12:50 -0500

    update next.config.mjs
    trying to get the files to be read :( it's the entire reason for this site

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    outputFileTracingIncludes: {
+        '/api/another': ['./necessary-folder/**/*'],
+    },
     images: {
         remotePatterns: [new URL('https://cdn.bladewyrm.dev/images/**')],
     }
